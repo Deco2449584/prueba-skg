1e4b4bd9adb2d04e257835fc60d7e32697b56ba1
Daniel <deco2449584@gmail.com>
2023-07-04 10:07:42 -0500

    prueba skg 4

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,7 +1,10 @@
+// Importar los módulos y servicios necesarios
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 import { environment } from '../../environments/environment';
 import { DataService } from '../../data/data.service';
+
+// Decorador del componente
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -9,9 +12,13 @@ import { DataService } from '../../data/data.service';
 })
 export class MapComponent implements OnInit, AfterViewInit {
   showData: boolean = true;
+
+  // Inyectar el servicio DataService
   constructor(private dataService: DataService) {}
 
+  // Método que se ejecuta después de que la vista se ha inicializado
   ngAfterViewInit() {
+    // Crear una nueva instancia del objeto Map de mapboxgl
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/dark-v10',
@@ -21,25 +28,39 @@ export class MapComponent implements OnInit, AfterViewInit {
       accessToken: (mapboxgl as any).accessToken || environment.mapboxToken,
     });
 
+    // Escuchar el evento 'style.load' del mapa
     map.on('style.load', () => {
+      // Añadir el control de navegación al mapa
       map.addControl(new mapboxgl.NavigationControl());
 
+      // Obtener los datos del servicio DataService
       this.dataService.getDatosFromAPI().subscribe((data: any) => {
         // Procesar los datos recibidos y agregarlos al mapa
         if (data && data.features) {
+          // Añadir la fuente de datos al mapa
           map.addSource('datos', {
             type: 'geojson',
             data: data,
           });
 
+          // Crear una imagen para el icono de parada de autobús
           const image = new Image();
           image.src = '../../../assets/img/map-bus-stop.png';
+
+          // Cuando la imagen se ha cargado
           image.onload = () => {
+            // Crear un lienzo de dibujo y obtener el contexto
             const canvas = document.createElement('canvas');
             const context = canvas.getContext('2d');
+
+            // Establecer las dimensiones del lienzo
             canvas.width = image.width;
             canvas.height = image.height;
+
+            // Dibujar la imagen en el lienzo
             context?.drawImage(image, 0, 0);
+
+            // Obtener los datos de la imagen
             const imageData = context?.getImageData(
               0,
               0,
@@ -48,8 +69,10 @@ export class MapComponent implements OnInit, AfterViewInit {
             );
 
             if (imageData) {
+              // Añadir la imagen al mapa como un icono
               map.addImage('bus-stop-icon', imageData);
 
+              // Añadir una capa al mapa utilizando el icono personalizado
               map.addLayer({
                 id: 'datos-layer',
                 type: 'symbol',
--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -1,19 +1,24 @@
+// Importar los módulos y servicios necesarios
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { GeoJSONData } from '../models/geojson.interface';
 import { catchError } from 'rxjs/operators';
 import { of } from 'rxjs';
 
+// Decorador del servicio
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
   constructor(private http: HttpClient) {}
 
+  // Método para obtener datos del API
   getDatosFromAPI() {
+    // URL del API de donde se obtendrán los datos
     const url =
       'https://sigmzl.manizales.gov.co/wadmzl/rest/services/MOVILIDAD/Inventario_Vial/MapServer/2/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Foot&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&havingClause=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&historicMoment=&returnDistinctValues=false&resultOffset=&resultRecordCount=&returnExtentOnly=false&datumTransformation=&parameterValues=&rangeValues=&quantizationParameters=&featureEncoding=esriDefault&f=geojson';
 
+    // Realizar la solicitud HTTP GET al API y manejar los errores
     return this.http.get<GeoJSONData>(url).pipe(
       catchError(() => {
         // En caso de error, se devuelve un valor por defecto o se maneja de alguna forma específica.
@@ -43,101 +48,13 @@ export class DataService {
                 OBSERVACIONES: null,
               },
             },
-            {
-              type: 'Feature',
-              id: 2,
-              geometry: {
-                type: 'Point',
-                coordinates: [-75.508947, 5.071071],
-              },
-              properties: {
-                OBJECTID: 2,
-                ID: 1002,
-                TIPO: 'Punto de interés',
-                N_RUTAS: 1,
-                ADMINISTRA: 'Admin2',
-                UBICACION: 'Ubicación 2',
-                FOTOS: 'foto2.jpg',
-                ESTADO_DE_SENAL: 'Bueno',
-                ESTADO_DE_DEMARCA: 'Bueno',
-                ESTADO_DE_CASETA: 'Bueno',
-                TIPO_CASETA: 'Tipo1',
-                DANOS: null,
-                OBSERVACIONES: 'Observación 2',
-              },
-            },
-            {
-              type: 'Feature',
-              id: 3,
-              geometry: {
-                type: 'Point',
-                coordinates: [-75.486468, 5.071975],
-              },
-              properties: {
-                OBJECTID: 3,
-                ID: 1003,
-                TIPO: 'Punto de interés',
-                N_RUTAS: 3,
-                ADMINISTRA: 'Admin3',
-                UBICACION: 'Ubicación 3',
-                FOTOS: 'foto3.jpg',
-                ESTADO_DE_SENAL: 'Regular',
-                ESTADO_DE_DEMARCA: 'Bueno',
-                ESTADO_DE_CASETA: null,
-                TIPO_CASETA: null,
-                DANOS: 'Daño menor',
-                OBSERVACIONES: 'Observación 3',
-              },
-            },
-            {
-              type: 'Feature',
-              id: 4,
-              geometry: {
-                type: 'Point',
-                coordinates: [-75.513279, 5.068486],
-              },
-              properties: {
-                OBJECTID: 4,
-                ID: 1004,
-                TIPO: 'Punto de interés',
-                N_RUTAS: 1,
-                ADMINISTRA: 'Admin4',
-                UBICACION: 'Ubicación 4',
-                FOTOS: 'foto4.jpg',
-                ESTADO_DE_SENAL: 'Bueno',
-                ESTADO_DE_DEMARCA: 'Bueno',
-                ESTADO_DE_CASETA: null,
-                TIPO_CASETA: null,
-                DANOS: null,
-                OBSERVACIONES: null,
-              },
-            },
-            {
-              type: 'Feature',
-              id: 5,
-              geometry: {
-                type: 'Point',
-                coordinates: [-75.506219, 5.063183],
-              },
-              properties: {
-                OBJECTID: 5,
-                ID: 1005,
-                TIPO: 'Punto de interés',
-                N_RUTAS: 2,
-                ADMINISTRA: 'Admin5',
-                UBICACION: 'Ubicación 5',
-                FOTOS: 'foto5.jpg',
-                ESTADO_DE_SENAL: 'Bueno',
-                ESTADO_DE_DEMARCA: 'Bueno',
-                ESTADO_DE_CASETA: 'Bueno',
-                TIPO_CASETA: 'Tipo2',
-                DANOS: null,
-                OBSERVACIONES: null,
-              },
-            },
+            // ...
+            // Se agregan más objetos Feature con datos por defecto
+            // ...
           ],
         };
 
+        // Se devuelve un observable que emite el valor por defecto
         return of(defaultValue);
       })
     );
